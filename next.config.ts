ac16929a2bf236bcb4675edb41f45925a91e18d3
Iury Bernardes <bernardes.iury@gmail.com>
2026-03-14 14:26:09 -0300

    Fix webhook

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,7 @@ const securityHeaders = [
 ];
 
 const nextConfig: NextConfig = {
+	skipTrailingSlashRedirect: true,
 	async headers() {
 		return [{ source: "/(.*)", headers: securityHeaders }];
 	},
