844314dddc9a3afb70f5bea59ec4b5d9c9faacbe
Iury Bernardes <bernardes.iury@gmail.com>
2026-03-14 07:54:57 -0300

    Add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,24 +82,28 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
+				<Script
+					id="gtm-script"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WZP6DNR2');`,
+					}}
+				/>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-WZP6DNR2"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					/>
+				</noscript>
 				<ThemeProvider>
 					<QueryProvider>{children}</QueryProvider>
 				</ThemeProvider>
-				<Script
-					async
-					src="https://www.googletagmanager.com/gtag/js?id=G-H254XDCQ0H"
-				/>
-				<Script
-					id="google-analytics"
-					dangerouslySetInnerHTML={{
-						__html: `
-							window.dataLayer = window.dataLayer || [];
-							function gtag(){dataLayer.push(arguments);}
-							gtag('js', new Date());
-							gtag('config', 'G-H254XDCQ0H');
-						`,
-					}}
-				/>
 			</body>
 		</html>
 	);
